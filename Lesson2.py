62f45f1e5689267a4f6f2b1e6fc97757a9ea1623
Shawn Oakley <shawn.oakley@dominodatalab.com>
2020-04-30 01:10:54 -0400

    Adds image data example

--- Lesson2.py
+++ Lesson2.py
@@ -171,3 +171,25 @@ writer.write(serialized_dataset)
 
 filenames = [filename]
 raw_dataset = tf.data.TFRecordDataset(filenames)
+
+# Loading Images
+import matploylib.pyplot as plt
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+def plot_image(image):
+    plt.figure()
+    plt.imshow(image)
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+
+plot_image(train_images[0])
+plot_image(train_images[1])
+
+# Normalize, scaled to between 0-1
+train_images = train_images / 255.0
+
+# Do the same for test images
+test_images = test_images / 255.0
