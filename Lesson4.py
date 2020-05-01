a93a5b0d6ef28aab08569f1644034c6f41adcacf
Shawn Oakley <shawn.oakley@dominodatalab.com>
2020-05-01 01:37:31 -0400

    Adds batching, etc

--- Lesson4.py
+++ Lesson4.py
@@ -35,3 +35,72 @@ train_stats = train_stats.transpose()
 normed_train_data = norm(train_dataset, train_stats)
 # Normalize test data
 normed_test_data  = norm(test_dataset, train_stats)
+
+# Build out Keras Sequential API
+model = keras.Sequential([
+    keras.layers.Dense(64, activation='relu', input_shape=[len(normed_train_data.keys())]),
+    keras.layers.Dense(64, activation='relu')
+    keras.layers.Dense(1)
+])
+
+optimizer = model.optimizers.RMSprop(0.001)
+model.compile(loss='mse',
+              optimize=optimizer,
+              metrics=['mse', 'mae'])
+
+model.fit(normed_train_data, train_labels, epochs=10)
+
+y_hats = model.predict(normed_test_data)
+y_hats = [x[0] for x in y_hats]
+keras.losses.MAE(
+    test_labels, y_hats
+)
+
+model.evaluate(normed_test_data, test_labels)
+
+# BATCHING AND PREFETCHING
+# Batching: split dataset up into multiple pieces
+# Prefetching: reads data off of disk before needed by model.
+# Alleviates bottleneck
+
+# batch by adding the batch method
+# prefetch 2 gives you 2 batches at a time, so next batch
+# is already loaded
+dataset = tf.data.Dataset.from_tensor_slices(
+    (normed_train_data.values, train_labels.values)
+).batch(100).prefetch(2)
+# Each epoch is a full evaluation of all the batches
+model.fit(dataset, epoch=100)
+
+# PARALLELIZING DATA EXTRACTION
+# Data stored remotely requires different processing
+# than data stored locally
+
+# Time-to-first-byte: Reading the first byte of data
+# from a remote storage location can take orders of
+# magnitude longer than from local storage
+# Read throughput: Remote storage can offer large bandwidth
+# but reading a single file might only be able
+# to use a fraction of this bandwidth
+
+# Interleave: Tensorflow transformation used to parallelize
+# the data loading step
+# Can specify the number of datasets to overlap (the interleaving), and the level of parallelism
+
+# EXAMPLE:
+tf.data.Dataset.interleave(
+    dataset,
+    num_parallel_calls=tf.data.experimental.AUTOTUNE
+)
+
+# Parallelizing interleave
+# Uses 'num_parallel_calls' argument to specify the level of parallelism
+# and load multiple datasets in parallel
+# 'tf.data.experimental.AUTOTUNE' delegates the decision
+# about what level of parallelism to use at runtime
+
+# BEST PRACTICES FOR PIPELINE PERFORMANCE
+# .batch -- Separate data into smaller btches rather than a
+# single dataset
+# .prefetch -- Overlap the work of loading data and model training
+# .interleave -- Make data reads and transformations run in parallel
